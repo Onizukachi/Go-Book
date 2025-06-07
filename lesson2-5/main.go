2eaae5ebd18bbe33161666b78788bc9aee054b6a
Alexei <dark_sao@mail.ru>
2025-06-07 12:39:10 +0300

    anagram checker

--- lesson2-5/main.go
+++ lesson2-5/main.go
@@ -7,9 +7,42 @@ import (
 )
 
 func main() {
+	commaRun()
+	isAnagramRun()
+}
+
+func isAnagramRun() int {
+	fmt.Println(isAnagram("hello", "word"))  // false
+	fmt.Println(isAnagram("Липа", "пила"))   // true
+	fmt.Println(isAnagram("Актер", "терка")) // true
+	fmt.Println(isAnagram("Сосна", "Насос")) // true
+
+	return 0
+}
+
+func isAnagram(str1, str2 string) bool {
+	counter := make(map[rune]int)
+	for _, el := range strings.ToLower(str1) {
+		counter[el]++
+	}
+
+	for _, el := range strings.ToLower(str2) {
+		counter[el]--
+
+		if counter[el] < 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func commaRun() int {
 	fmt.Println(comma("-12345321789.231231")) // -12,345,321,789.231231
 	fmt.Println(comma("-12345321789"))        // -12,345,321,789
 	fmt.Println(comma("12345321789.23"))      // 12,345,321,789.23
+
+	return 0
 }
 
 func comma(s string) string {
